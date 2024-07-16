e9eabf21091761737df691db18539b34b7c02b0a
jarutosurano <jarutosurano@gmail.com>
2024-07-16 13:24:53 -0400

    file co location

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const settings = () => {
+    return (
+      <div>settings</div>
+    )
+  }
+  
+export default function page() {
+  return (
+    <div>page</div>
+  )
+}
