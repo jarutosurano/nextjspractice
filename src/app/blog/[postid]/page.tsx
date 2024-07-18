123b4df0991570c5ecb4f279f285e75ffeed9208
jarutosurano <jarutosurano@gmail.com>
2024-07-18 14:28:41 -0400

    add metadata routing

--- src/app/blog/[postid]/page.tsx
+++ src/app/blog/[postid]/page.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Metadata } from 'next';
 
-const page = ({params}:{
-    params:{postid: string}
-}) => {
+type Props = {
+  params: {
+    postid: string;
+  };
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Blog Post ${params.postid}`
+  };
+};
+
+export default function Page({ params }: Props) {
   return (
     <div>
-        <h1>Details Page{params.postid}</h1>
+      <h1>Details page with an ID: {params.postid}</h1>
     </div>
-  )
+  );
 }
-
-export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="h-screen">
+      <h1 className="text-4xl">Homepage</h1>
     </div>
   )
   }
